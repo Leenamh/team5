1745da772066defdffd3c5923ec889f98d0f0b35
leena <LeenaAlmusharraf@icloud.com>
2025-09-29 17:11:56 +0300

    Initial Commit

---
+++ clueApp/clueAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  clueAppApp.swift
+//  clueApp
+//
+//  Created by leena almusharraf on 29/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct clueAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
