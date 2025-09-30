cbb10abd63142917690d26ad3c3e096a238ba7c7
Noura <Noura5nk@gmail.com>
2025-09-30 15:39:02 +0300

    NumOtionsView page

---
+++ clueApp/RandomeHome.swift
@@ -0,0 +1,62 @@
+//
+//  RandomeHome.swift
+//  clueApp
+//
+//  Created by Norah Khalid on 30/09/2025.
+//
+
+
+import SwiftUI
+
+struct RandomeHome: View {
+    var body: some View {
+        NavigationView {
+            VStack (spacing: 30){
+           
+                Spacer()
+                    .padding(.horizontal)
+               
+                    HStack {
+                        
+                        
+                        Image("Image 3")
+                            .padding(.trailing)
+                            
+                        
+                    }
+                    Spacer()
+                    
+                    Image("Image 2")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 400, height: 400)
+                    
+                    
+                }
+                .padding()
+                
+                
+                .toolbar {
+                    
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button("Done") {
+                           
+                        }
+                        .font(.system(size: 18, weight: .medium, design: .rounded))
+                        .foregroundColor(Color(red: 1.0, green: 116/255, blue: 114/255))
+                    }
+                    
+                    
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button("Add") {
+                            
+                        }.font(.system(size: 18, weight: .medium, design: .rounded))
+                            .foregroundColor(Color(red: 1.0, green: 116/255, blue: 114/255))
+                    }}}
+                    }
+                }
+            
+            #Preview {
+                RandomeHome()
+            }
+        
